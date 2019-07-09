e01f1cf876723c27e7da27e15d6c3aa8a41d5437
furkandeveloper <Furkan.dvlp@gmail.com>
2019-07-09 16:01:43 +0300

    whatsapp ana ekran tasarımının oluşturulması

--- whatsapp_clone/lib/screens/homeScreen.dart
+++ whatsapp_clone/lib/screens/homeScreen.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=>HomeScreenState();
+  State<StatefulWidget> createState() => HomeScreenState();
 }
 
-class HomeScreenState extends State<HomeScreen>{
+//SingleTickerProviderStateMixin vsync 'nin aktif olması için mixin edildi
+class HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, initialIndex: 1, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("WhatsApp",textDirection: TextDirection.ltr,),
-      ),
-      body: Container(
-        child: Center(
-          child: Text("Hello"),
+        title: Text(
+          "WhatsApp",
+          textDirection: TextDirection.ltr,
         ),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(text: "Sohbet"),
+            Tab(text: "Durum"),
+            Tab(text: "Aramalar"),
+          ],
+        ),
+        actions: <Widget>[Icon(Icons.search), Icon(Icons.more_vert)],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Text("Kamera"),
+          Text("Sohbet"),
+          Text("Durum"),
+          Text("Aramalar")
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        onPressed: () {},
+        tooltip: "Sohbet",
+        child: Icon(Icons.message,
+        color: Colors.white,),
       ),
     );
   }
-
 }
